610197c670a354e060bd61b56079dec7a4a1c45d
Klaytheist <71794778+Klaytheist@users.noreply.github.com>
2021-05-18 15:36:59 +0530

    Create patterns.c

---
+++ patterns.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void) { 
+    int type, rows, i, j;
+    type = get_int("Enter type of pattern (1 or 2) ");
+    rows = get_int("Number of rows ");
+    if(type == 1)
+    {
+        for(i = rows; i > 0; i--)
+        {   
+            for(j = i; j > 0; j--)
+            {
+                printf("#");
+            }
+            printf("\n");
+         }
+     }
+     if(type == 2)
+     { 
+         for(i = 0; i < rows ; i++)
+         {
+             for(int k = rows - i - 1; k > 0; k--)
+             {
+                 printf(" ");
+             }
+             for(j = 0; j <= i; j++)
+             {
+                 printf("#");
+             } 
+             printf("\n");
+         }
+     } 
+}
